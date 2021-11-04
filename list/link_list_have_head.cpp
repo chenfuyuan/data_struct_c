bacfb71bd4a8c21d4456c3269f3042f8c9499e1f
chenfuyuan <chenfuyuan0713@163.com>
2021-11-04 22:44:28 +0800

    feat(list): 带有头节点的单链表，进行按位序插入

--- list/link_list_have_head.cpp
+++ list/link_list_have_head.cpp
@@ -6,7 +6,7 @@
 typedef int Element;
 
 typedef struct LNode{
-    Element LNode;
+    Element data;
     struct LNode *next;
 }*LinkList,LNode;
 /*等价于
@@ -19,12 +19,82 @@ typedef struct LNode *LinkList;    //线性表
  */
 
 //定义函数
-bool InitList(LinkList&);
-
+bool InitList(LinkList&);    //初始化线性表
+bool ListInsert(LinkList&,int,Element);    //插入数据
+void printError(bool,char*);
+bool printList(LinkList);
 int main(){
     LinkList list;
     InitList(list);
+    bool isSuccess = ListInsert(list, 1, 1);
+    isSuccess = ListInsert(list, 2, 2);
+    printError(isSuccess, "插入");
+    printList(list);
+
     return 0;
+}
+
+bool printList(LinkList list){
+    if (list == NULL) {
+        return false;
+    }
+    LNode *p;
+    p = list->next;
+    while(p!=NULL){
+        printf("%d ", p->data);
+        p=p->next;
+    }
+
+    return true;
+
+}
+
+void printError(bool b,char* msg){
+    if(!b){
+        printf("%s失败\n", msg);
+    }
+}
+
+/**
+ * 获取的一个新节点
+ * @return 一个新节点
+ */
+LNode* getNewNode(){
+    return (LNode *) malloc(sizeof(LNode));
+}
+
+/**
+ * 向表list的第index个位置，插入元素element
+ * @param list 线性表
+ * @param i 插入位置
+ * @param element 插入元素
+ * @return 是否插入成功
+ */
+bool ListInsert(LinkList &list, int i, Element element){
+    if(i < 1) {
+        return false;
+    }
+    LNode *p;
+    int j=0;
+    p = list;
+    //将指针移动到 i-1 位置
+    while(p != NULL && j < i - 1){
+        p = p->next;
+        j++;
+    }
+
+    //插入位置超过 链表长度
+    if(p==NULL) {
+        return false;
+    }
+
+    //插入元素
+    LNode *node = getNewNode();   //初始化节点
+    node->data = element;    //赋值元素
+    node->next = p->next;    //将原本在i位置的节点，链接到新节点的后面
+    p->next = node;    //将新节点链接到i位置
+
+    return true;
 }
 
 /**
@@ -33,7 +103,7 @@ int main(){
  * @return 空的线性表
  */
 bool InitList(LinkList &L){
-    L = (LNode *)malloc(sizeof(LNode));
+    L = getNewNode();
     if (L == NULL) {    //内存不足，分配失败
         return false;
     }
