39f38552a0ca5fc62add097840ba918d7c2386d7
chenfuyuan <chenfuyuan0713@163.com>
2021-11-17 13:59:57 +0800

    feat(stack): 使用栈实现中缀表达式计算，并把中序表达式转化为后缀表达式

--- stack_queue/link_stack.cpp
+++ stack_queue/link_stack.cpp
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
-typedef char ElemType;
+typedef int ElemType;
 typedef struct Node{
     ElemType data;
     Node *next;    //指向下一个结点
@@ -13,6 +13,22 @@ bool GetTop(LinkStack, ElemType &);    //获取栈顶元素
 bool StackEmpty(LinkStack);    //判断栈是否为空
 bool PrintStack(LinkStack);    //打印栈
 Node* GetNode();    //获取一个新结点
+
+/**
+ * 获取栈顶元素
+ * @param stack 栈
+ * @param elem 元素
+ * @return 是否获取成功
+ */
+bool GetTop(LinkStack stack,ElemType &elem){
+    if (StackEmpty(stack)) {
+        return false;
+    }
+
+    elem = stack->data;    //获取栈顶元素
+
+    return true;
+}
 
 /**
  * 测试用例-测试所有方法
@@ -169,9 +185,299 @@ void test_bracket_match(){
     printf("匹配成功。\n");
 }
 
+
+/**
+ * 判断字符是否为数字
+ * @param ch 数字
+ * @return
+ */
+bool isNum(char ch){
+    return ch >= '0' && ch <= '9';
+}
+
+
+/**
+ * 假设字符ch为数字
+ * 将字符ch转换为数字int类型
+ * @param ch 需要转换的字符
+ * @return 转换结果
+ */
+int char_to_num(char ch){
+    return (int)(ch - '0');
+}
+
+/**
+ * 判断字符是否为运算符
+ * @param ch 字符
+ * @return 如果字符为运算符则返回true
+ */
+bool isOpt(char ch){
+    return ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '%';
+}
+
+/**
+ * 如果运算符opt_01 优先于 运算符opt_02 则返回true
+ * @param opt_01 运算符01
+ * @param opt_02 运算符02
+ * @return 运算符opt_01是否比 运算符opt_02优先
+ */
+bool isOptPrior(char opt_01, char opt_02){
+    if (opt_01 == '+' || opt_01 == '-') {
+        return true;
+    }
+
+    if (opt_02 == '*' || opt_02 == '/' || opt_02 == '%') {
+        return true;
+    }
+
+    return false;
+}
+
+/**
+ * 进行运算
+ * left_num opt right_num = result
+ * @param opt 运算符
+ * @param left_num 左操作数
+ * @param right_num 右操作数
+ * @return 运算是否成功
+ */
+bool opt_calculate(char opt,int left_num,int right_num,int &result){
+    //根据操作数，进行不同的运算
+    switch (opt) {
+        case '+':
+            result = left_num + right_num;
+            break;
+        case '-':
+            result = left_num - right_num;
+            break;
+        case '*':
+            result = left_num * right_num;
+            break;
+        case '/':
+            result = left_num / right_num;
+            break;
+        case '%':
+            result = left_num % right_num;
+            break;
+        default:
+            return false;
+    }
+
+    return true;
+}
+
+/**
+ * 计算中序表达式 并将值赋值到result变量中
+ * @param str 中序表达式
+ * @param result 值
+ * @return
+ */
+bool medium_expression_calculate(char *str,int &result){
+    LinkStack opt_stack;    //运算符栈
+    LinkStack num_stack;    //操作数栈
+    InitStack(opt_stack);
+    InitStack(num_stack);
+    while (true) {
+        char ch = *str;
+        if (ch == '\0') {
+            break;
+        }
+        int num = 0;
+        //当字符为'-'时且下一个字符为数字时  或 字符为数字时
+        if(isNum(ch)) {
+            //获取接下来的字符，构建成操作数
+            while (true) {
+                num *= 10;
+                num += char_to_num(ch);
+                if (!isNum(*(str + 1))) {
+                    break;
+                }
+
+                //跳转到下一个字符
+                ch = *(++str);
+                if (ch == '\0') {
+                    break;
+                }
+            }
+
+            //将操作数入栈
+            printf(" %d ", num);
+            Push(num_stack, num);
+        }
+
+        if (isOpt(ch)) {    //如果字符为运算符
+            while (!StackEmpty(opt_stack)) {
+                ElemType topElem = -1;
+                GetTop(opt_stack, topElem);
+                char opt = char(topElem);
+                if (!isOptPrior(ch, opt) || is_left_bracket(opt)) {
+                    break;
+                }
+                printf(" %c ", opt);
+                Pop(opt_stack, topElem);
+                //从操作数栈中，取出2个操作数，进行计算
+                int right_opt_num = -1;
+                int left_opt_num = -1;
+
+                //获取右操作数
+                bool isSuccess = Pop(num_stack, right_opt_num);
+                if (!isSuccess) {
+                    printf("系统错误，获取右操作数时，失败!\n");
+                    return false;
+                }
+
+                //获取左操作数
+                isSuccess = Pop(num_stack, left_opt_num);
+                if (!isSuccess) {
+                    printf("系统错误，获取左操作数时，失败!\n");
+                    return false;
+                }
+                int calculate_result = -1;
+                isSuccess = opt_calculate(opt, left_opt_num, right_opt_num,calculate_result);
+                if (!isSuccess) {
+                    printf("系统错误，运算错误!\n");
+                    return false;
+                }
+                Push(num_stack, calculate_result);
+            }
+            Push(opt_stack, ch);
+        }
+
+        if (is_left_bracket(ch)) {
+            Push(opt_stack, ch);
+        }
+
+        if (is_right_bracket(ch)) {
+            //获取操作数栈顶元素，
+            ElemType topElem;
+            bool isSuccess = Pop(opt_stack, topElem);
+            if (!isSuccess) {
+                printf("系统错误，获取运算符失败!\n");
+                return false;
+            }
+            char opt = char(topElem);
+            printf(" %c ", opt);
+            //从操作数栈中，取出2个操作数，进行计算
+            int right_opt_num = -1;
+            int left_opt_num = -1;
+
+            //获取右操作数
+            isSuccess = Pop(num_stack, right_opt_num);
+            if (!isSuccess) {
+                printf("系统错误，获取右操作数时，失败!\n");
+                return false;
+            }
+
+            //获取左操作数
+            isSuccess = Pop(num_stack, left_opt_num);
+            if (!isSuccess) {
+                printf("系统错误，获取左操作数时，失败!\n");
+                return false;
+            }
+            int calculate_result = -1;
+            isSuccess = opt_calculate(opt, left_opt_num, right_opt_num,calculate_result);
+            if (!isSuccess) {
+                printf("系统错误，运算错误!\n");
+                return false;
+            }
+            Push(num_stack,calculate_result);
+            if (!GetTop(opt_stack, topElem)) {
+                printf("系统错误 获取栈顶元素失败!\n");
+                return false;
+            }
+            char bracket = char(topElem);
+            if (!is_left_bracket(bracket)) {
+                printf("系统错误，括号不匹配!\n");
+                return false;
+            }
+            Pop(opt_stack, topElem);
+        }
+
+        str+=1;
+    }
+
+    while (!StackEmpty(opt_stack)) {
+        if (StackEmpty(num_stack)) {
+            printf("系统错误，操作数栈耗尽。\n");
+            return false;
+        }
+
+        ElemType topElem;
+        bool isSuccess = Pop(opt_stack, topElem);
+        if (!isSuccess) {
+            printf("系统错误,获取运算符错误。\n");
+            return false;
+        }
+        char opt = (char) topElem;
+        printf(" %c ", opt);
+        if (!isOpt(opt)) {
+            printf("系统错误，运算符栈元素非运算符。\n");
+            return false;
+        }
+
+        //从操作数栈中，取出2个操作数，进行计算
+        int right_opt_num = -1;
+        int left_opt_num = -1;
+
+        //获取右操作数
+        isSuccess = Pop(num_stack, right_opt_num);
+        if (!isSuccess) {
+            printf("系统错误，获取右操作数时，失败!\n");
+            return false;
+        }
+
+        //获取左操作数
+        isSuccess = Pop(num_stack, left_opt_num);
+        if (!isSuccess) {
+            printf("系统错误，获取左操作数时，失败!\n");
+            return false;
+        }
+        int calculate_result = -1;
+        isSuccess = opt_calculate(opt, left_opt_num, right_opt_num,calculate_result);
+        if (!isSuccess) {
+            printf("系统错误，运算错误!\n");
+            return false;
+        }
+        Push(num_stack,calculate_result);
+    }
+
+    if (StackEmpty(num_stack)) {
+        printf("系统错误,计算没有结果\n");
+        return false;
+    }
+
+    Pop(num_stack, result);
+    if (!StackEmpty(num_stack)) {
+        printf("系统错误,计算未完成\n");
+        return false;
+    }
+    printf("\n");
+    return true;
+}
+
+/**
+ * 测试中序表达式求职
+ */
+void test_medium_expression(){
+    int calculate_result = -1;
+    char input[30];
+    scanf("%s", input);
+    bool isSuccess = medium_expression_calculate(input, calculate_result);
+    if (!isSuccess) {
+        printf("计算错误\n");
+        return;
+    }
+    printf("运算结果:%d", calculate_result);
+}
+
 int main() {
     //test_all_function();
-    test_bracket_match();
+
+    //测试括号匹配
+    //test_bracket_match();
+
+    //printf("%d", char_to_num('0'));
+    test_medium_expression();
 }
 
 /**
